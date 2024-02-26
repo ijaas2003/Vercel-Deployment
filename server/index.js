56af999f9bd85201840ed532ec3266981a6e2fb7
ijaasM4 <98891153+ijaasM4@users.noreply.github.com>
2024-02-26 15:59:10 +0530

    updated new

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
 	res.send("Server is running on Port 4000");
-})
+});
+const mongoURI = "mongodb+srv://ijaas:ijaas@cluster0.httu3xq.mongodb.net/"
+mongoose.connect(mongoURI)
+.then(() => {
+    console.log('Connected to MongoDB Atlas');
+});
 
 app.listen(4000, () => {
 	console.log("Server is running on port", 4000)
